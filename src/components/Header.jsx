4c168156e8dfd5c18125b6b166dfc0b3edeaaef8
Yulia <yulia.rahulina@gmail.com>
2023-08-07 21:20:43 +0300

    bootstrap carousel

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -24,6 +24,35 @@ function Header() {
                         <div className="selfItem bg8"></div>
                     </div>
                 </div>
+                <div className="carContainer">
+                    <div id="carouselExampleFade" className="carousel slide carousel-fade" data-bs-ride="carousel">
+                        <div className="carousel-inner">
+                            <div className="carousel-item active">
+                                <img src={bgCar1} className="d-block w-100" alt="..."/>
+                            </div>
+                            <div className="carousel-item">
+                                <img src={bgCar2} className="d-block w-100" alt="..."/>
+                            </div>
+                            <div className="carousel-item">
+                                <img src={bgCar3} className="d-block w-100" alt="..."/>
+                            </div>
+                            <div className="carousel-item">
+                                <img src={bgCar4} className="d-block w-100" alt="..."/>
+                            </div>
+                        </div>
+                        <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleFade"
+                                data-bs-slide="prev">
+                            <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+                            <span className="visually-hidden">Previous</span>
+                        </button>
+                        <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleFade"
+                                data-bs-slide="next">
+                            <span className="carousel-control-next-icon" aria-hidden="true"></span>
+                            <span className="visually-hidden">Next</span>
+                        </button>
+                    </div>
+                </div>
+            </div>
             <div className="greenBar">
                 <div className="green"></div>
             </div>
